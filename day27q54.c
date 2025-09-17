716092256023803b1cba3bb8fdc193ae6e065ba1
Sahaj9238 <sahajparikh2007@gmail.com>
2025-09-17 16:40:22 +0530

    day27q54.c

--- day27q54.c
+++ day27q54.c
@@ -1,35 +1,34 @@
 /*Write a program to print the following pattern:
-
-*
-
-***
-
-*****
+   *
+  ***
+ *****
 *******
-***
-
-*
-*/
+ *****
+  ***
+   *
+ */
 #include <stdio.h>
-int main(){
-    
-    for (int i = 1; i <= 8; i += 2) {
-        for (int j = 1; j <= i; j++) {
-            printf("*");
+int main() {
+    char c='*';
+    for(int i=1;i<=8;i+=2){
+        for (int k=1;k<=8-i;k++){
+            printf(" ");
         }
-        if (i<=4){
-            printf("\n");
-            printf("\n"); 
-        }
-        if (i==5||i==7){
-            printf("\n");
-        }
-    }
-    for (int i=3;i>=1;i-=2){
         for (int j=1;j<=i;j++){
-            printf("*");
+            printf(" %c",c);
+            
         }
         printf("\n");
+    }
+    for(int i=5;i>=1;i-=2){
+        for (int k=1;k<=8-i;k++){
+            printf(" ");
+        }
+        for (int j=1;j<=i;j++){
+            printf(" %c",c);
+            
+        }
         printf("\n");
     }
+    return 0;
 }
